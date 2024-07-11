1758d36900d17b6c34fc1303d72a803f50bc7aa0
Ahmed-Naserelden <an2071497@gmail.com>
2024-07-11 15:42:27 +0300

    Edit: Defualt DATABASE_HOST

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ ENV PYTHONUNBUFFERED 1
 WORKDIR /app
 
 # Copy the current directory contents into the container at /code
-COPY . /app/
+COPY . .
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
--- wholesale/settings.py
+++ wholesale/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-DATABASE_HOST="db"
+DATABASE_HOST="localhost"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
