665821aeec7a7d3617b5eeb3e32d74880c43f856
Ahmed-Naserelden <an2071497@gmail.com>
2024-07-11 15:08:26 +0300

    Revert "fix: portability"
    This reverts commit c7aaeb11f151f1e580d90f16bdae15afc4443d6c.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
 # Apply database migrations
-echo "Changing DataBase settings ..."
-SETTINGS_FILE="wholesale/settings.py"
-VARIABLE_NAME="DATABASE_HOST"
-NEW_VALUE="db"
-
-sed -i "s/\($VARIABLE_NAME *= *\).*/\1\"$NEW_VALUE\"/" $SETTINGS_FILE
-echo "$VARIABLE_NAME updated to $NEW_VALUE in $SETTINGS_FILE"
-
 echo "Applying database migrations..."
 python manage.py migrate
 
